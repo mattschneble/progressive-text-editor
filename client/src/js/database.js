da6279adf95301304d5ba41c892bba90fb58a959
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-28 18:50:10 -0700

    finished database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.log('PUT to the database');
 // Create a connection to the database and version we want to use
 const jateDB = await openDB('jate', 1);
 // Create a transaction on the database
@@ -26,8 +26,24 @@ const request = store.put({ id: 1, text: content});
 const result = await request;
 // Console log the result with a message
 console.log('Successfully added content to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.log('GET from the database');
+// Create a connection to the database and version we want to use
+const jateDB = await openDB('jate', 1);
+// Create a transaction on the database
+const tx = jateDB.transaction('jate', 'readonly');
+// Get the object store we want to use
+const store = tx.objectStore('jate');
+// Use the get method to only return one entry from the database
+const request = store.get(1);
+// Wait for the request to complete
+const result = await request;
+// Console log the result 
+console.log('result.value', result);
+// Return the result
+return result?.text;
+};
 
 initdb();
