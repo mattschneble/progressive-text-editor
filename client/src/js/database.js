a065a13efa46218aded0731fbc8da6d71cd7a3f0
mattschneble <136631410+mattschneble@users.noreply.github.com>
2023-08-28 18:43:19 -0700

    added method to get some content in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,18 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+// Create a connection to the database and version we want to use
+const jateDB = await openDB('jate', 1);
+// Create a transaction on the database
+const tx = jateDB.transaction('jate', 'readwrite');
+// Get the object store we want to use
+const store = tx.objectStore('jate');
+// Use the put method to add the content
+const request = store.put({ id: 1, text: content});
+// Wait for the request to complete
+const result = await request;
+// Console log the result with a message
+console.log('Successfully added content to the database', result);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
